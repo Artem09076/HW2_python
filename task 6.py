8232536bddcdfa8aff2b7f4ae507daa227e3ed28
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-03-12 19:13:25 +0300

    Update task 6.py

--- task 6.py
+++ task 6.py
@@ -1,6 +1,6 @@
 n=int(input())
 factorial=1
-for i in range(1,n+1):
+for i in range(1,n+1):#Здесь мы считаем факториал числа путём умножения числа i на 1, после чего мы прибавим к факториалу 1(это по заданию)
     factorial*=i
 s=factorial+1
 print(s)
