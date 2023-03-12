868a8054be6b2f3a89b8205677f0ce403c1a6afa
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-03-12 18:15:31 +0300

    Update task 4.py

--- task 4.py
+++ task 4.py
@@ -1,27 +1,11 @@
-
-num = int(input())
-prev_num = num
-incr_len = 0 
-max_incr_len = 0 
-decr_len = 0 
-max_decr_len = 0 
-
-num = int(input())
-while num != 0:
-    if num > prev_num:
-        incr_len += 1
-        max_incr_len = max(max_incr_len, incr_len)
-        decr_len = 1
-    elif num < prev_num:
-        decr_len += 1
-        max_decr_len = max(max_decr_len, decr_len)
-        incr_len = 1
+n=int(input())
+s=n
+max_d=0
+while n!=0:
+    if n>s:
+        max_d+=1
+    elif n==s:
+        max_d=0
     else:
-        incr_len += 1
-        decr_len += 1
-
-    prev_num = num
-    num = int(input())
-
-max_len = max(max_incr_len, max_decr_len)
-print(max_len)
+        max_d=1
+print (max_d)
